01a4c0c35166a272f61a38851705b25552fcad99
mazzcris <cristiano.mazzarini@gmail.com>
2017-06-14 11:22:51 +0200

    Add icons + a few refines

--- web/app.js
+++ web/app.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
           var billableHours = parseFloat(event.title.split('||')[0]);
           var unBillableHours = parseFloat(event.title.split('||')[1]);
           return $('<a class="fc-day-grid-event fc-h-event fc-event fc-start fc-end event fc-draggable fc-resizable ' + event.className.join(' ') + '">' +
-              '<div class="fc-content"> <span class="fc-title">' + billableHours + ' [icon-here] <br> ' + unBillableHours + ' [icon-here]</span></div><div class="fc-resizer fc-end-resizer"></div>' +
+              '<div class="fc-content"> <span class="fc-title">' + billableHours + ' <img src="billable.svg"><br>' + unBillableHours + ' <img src="not-billable.svg"></span></div><div class="fc-resizer fc-end-resizer"></div>' +
               '</a>');
         },
         eventClick: function (data, event, view) {
