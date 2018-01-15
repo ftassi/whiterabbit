af40101d643a85d4c29e21eb7c28cdca2b444871
ricfrank <riccardo.franconi@gmail.com>
2018-01-15 16:40:37 +0100

    fix deploy script

--- idxfile.php
+++ idxfile.php
@@ -1,30 +1,33 @@
 <?php
 
-use Idephix\IdephixInterface;
+use Idephix\Context;
 
 /**
  * Deploy application
  */
-function deploy(IdephixInterface $idx, $go = false)
+function deploy(Idephix\Context $context, $go = false)
 {
     if (!$go) {
-        $idx->output->writeln('<comment>Dry Run</comment>');
+        $context->output()->writeln('<comment>Dry Run</comment>');
     }
 
     $dryRun = $go ? '' : '--dry-run';
-    $target = $idx->getCurrentTarget();
 
-    $user = $target->get('ssh_params.user');
-    $host = $idx->getCurrentTargetHost();
-    $remoteDir = $target->get('deploy.remote_dir');
-    $exclude = $target->get('deploy.rsync_exclude_file');
+    $user = $context->get('ssh_params.user');
+    $host = $context->currentHost();
 
-    $idx->local("rsync -rlDcz --force --delete --progress --exclude-from '$exclude' $dryRun -e 'ssh' . $user@$host:$remoteDir");
+    $remoteDir = $context->get('deploy.remote_base_dir');
+    $exclude = $context->get('deploy.rsync_exclude');
+
+    $context->local("rsync -rlDcz --force --delete --progress --exclude-from '$exclude' $dryRun -e 'ssh' . $user@$host:$remoteDir");
 
 }
 
-function devServer(IdephixInterface $idx)
+/**
+ * Run application for development
+ */
+function devServer(\Idephix\Context $context)
 {
-    $idx->output->writeln('<comment>Open http://localhost:8080/index.html</comment>');
-    $idx->local('php -S localhost:8080 -d display_errors=0 -t web');
+    $context->output()->writeln('<comment>Open http://localhost:8080/index.html</comment>');
+    $context->local('php -S localhost:8080 -d display_errors=0 -t web');
 }
--- idxrc.php
+++ idxrc.php
@@ -1,23 +1,20 @@
 <?php
 
-$targets = array(
+$environments = array(
     'prod' => array(
         'hosts' => array('52.57.245.198'),
         'ssh_params' => ['user' => 'ideato'],
         'deploy' => array(
-            'local_dir' => ".",
-            'remote_dir' => "/var/www/time.ideato.it",
-            'rsync_exclude_file' => 'deploy_exclude'
+            'remote_base_dir' => "/var/www/time.ideato.it",
+            'rsync_exclude' => 'deploy_exclude'
         ),
     ),
 );
 
-return \Idephix\Config::fromArray(
-    array(
-        'targets' => $targets,
-        'sshClient' => new \Idephix\SSH\SshClient(new \Idephix\SSH\CLISshProxy()),
-        'extension' => array(
-            'project' => new \Idephix\Extension\Project\Project(),
-        ),
-    )
-);
+return [
+    'envs' => $environments,
+    'ssh_client' => new \Idephix\SSH\SshClient(),
+    'extensions' => [
+        'rsync' => new \Idephix\Extension\Project\Rsync(),
+    ],
+];
